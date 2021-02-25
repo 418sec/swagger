38d942eba37c3ccd51baecfc925a0b88a424dd12
Abdul muhaimin <abdulmuhaiminkp030@gmail.com>
2021-02-25 11:44:58 +0530

    Update server.py

--- server/server.py
+++ server/server.py
@@ -196,7 +196,7 @@ def get_specs(req_path):
 
 def get_spec_index():
     with open(os.path.join(ROOT_DIR, "spec_index.yaml")) as f:
-        spec_index = yaml.load(f, Loader=yaml.FullLoader)
+        spec_index = yaml.load(f, Loader=yaml.SafeLoader)
     return spec_index
 
 
